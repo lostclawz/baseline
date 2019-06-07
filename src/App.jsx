0ae6f77c387caada1879c7925f69db37a70ad50f
Philip Kuperberg <philipkuperberg@gmail.com>
2019-06-07 13:57:32 -0400

    added suspense container for dynamically loaded components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import '~/style/style.scss';
 import React, { lazy } from 'react';
-import { hot } from 'react-hot-loader';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Store from '~/context/Store';
+import Loading from '~/components/Loading';
 // dynamically loaded components
 const Page = lazy(() => import('~/routes/Page'));
 const Home = lazy(() => import('~/routes/Home'));
@@ -10,12 +10,14 @@ const Home = lazy(() => import('~/routes/Home'));
 const App = () => (
    <Store>
       <BrowserRouter>
-         <Switch>
-            <Route path="/page" component={Page} />
-            <Route path="/" component={Home} />
-         </Switch>
+         <React.Suspense fallback={<Loading />}>
+            <Switch>
+               <Route path="/page" component={Page} />
+               <Route path="/" component={Home} />
+            </Switch>
+         </React.Suspense>
       </BrowserRouter>
    </Store>
 );
 
-export default hot(module)(App);
+export default App;
---
+++ src/components/Loading.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Loading = React.memo(() => <div className="loading" />);
+
+export default Loading;
