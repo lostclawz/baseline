fad790d24f42ce6509e8c50c7bfb91b805b1e4aa
Philip Kuperberg <philipkuperberg@gmail.com>
2019-06-07 13:48:47 -0400

    dynamically importing routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,18 @@
-import React from 'react';
+import '~/style/style.scss';
+import React, { lazy } from 'react';
 import { hot } from 'react-hot-loader';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Page from '~/routes/Page';
-import Home from '~/routes/Home';
-
-import '~/style/style.scss';
 import Store from '~/context/Store';
+// dynamically loaded components
+const Page = lazy(() => import('~/routes/Page'));
+const Home = lazy(() => import('~/routes/Home'));
 
 const App = () => (
    <Store>
       <BrowserRouter>
          <Switch>
-            <Route path="/page">
-               <Page />
-            </Route>
-            <Route path="/">
-               <Home />
-            </Route>
+            <Route path="/page" component={Page} />
+            <Route path="/" component={Home} />
          </Switch>
       </BrowserRouter>
    </Store>
