3afd6a4d94c12f5ad014194107911f1b0ae0fd8d
Philip Kuperberg <philipkuperberg@gmail.com>
2019-06-07 13:35:10 -0400

    linted dev webpack config

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,12 +1,16 @@
 const merge = require('webpack-merge');
 const WebpackNotifierPlugin = require('webpack-notifier');
 const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
-const PACKAGE = require('./package.json');
-const WEBPACK_DEV_SERVER_PORT = PACKAGE['app-ports']['webpack-port'];
-var WebpackShellPlugin = require('webpack-shell-plugin');
+const WebpackShellPlugin = require('webpack-shell-plugin');
+const autoprefixer = require('autoprefixer');
+const dartSass = require('sass');
+
 const common = require('./webpack.common.js');
+const PACKAGE = require('./package.json');
+
+const WEBPACK_DEV_SERVER_PORT = PACKAGE['app-ports']['webpack-port'];
 
 
 const SITE_TITLE = `${PACKAGE.name} [dev ${PACKAGE.version}]`;
@@ -46,23 +50,26 @@ module.exports = merge.smart(common, {
                   options: {
                      sourceMap: true,
                      url: false,
-                     
+
                   },
                },
                {
                   loader: 'postcss-loader',
                   options: {
                      sourceMap: true,
-                     plugins () {
-                        return [require('autoprefixer')];
+                     plugins() {
+                        return [autoprefixer];
                      },
                   },
                },
                { loader: 'resolve-url-loader', options: { sourceMap: true } },
-               { loader: 'sass-loader', options: {
-                  sourceMap: true,
-                  implementation: require('sass')
-               } },
+               {
+                  loader: 'sass-loader',
+                  options: {
+                     sourceMap: true,
+                     implementation: dartSass,
+                  },
+               },
             ],
          },
       ],
@@ -71,7 +78,7 @@ module.exports = merge.smart(common, {
       new webpack.HotModuleReplacementPlugin(),
       new HtmlWebpackPlugin({
          title: SITE_TITLE,
-         template: `${__dirname  }/src/index.html`,
+         template: path.join(__dirname, 'src', 'index.html'),
          chunksSortMode: 'none',
       }),
       new webpack.DefinePlugin({
